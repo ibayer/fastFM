74e891e0202c9de58a7e0aa2851c97c95923a031
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2015-07-04 20:40:07 +0100

    Update documentation

---
+++ _build/html/guide-1.py
@@ -0,0 +1,48 @@
+from fastFM import als
+from fastFM.datasets import make_user_item_regression
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+
+X, y, coef = make_user_item_regression(label_stdev=.4)
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+
+n_iter = 20
+step_size = 1
+l2_reg_w = 0
+l2_reg_V = 0
+
+fm = als.FMRegression(n_iter=0, l2_reg_w=0, l2_reg_V=0, rank=4)
+# Allocates and initalizes the model parameter.
+fm.fit(X_train, y_train)
+
+rmse_train = []
+rmse_test = []
+r2_score_train = []
+r2_score_test = []
+
+for i in range(1, n_iter):
+    fm.fit(X_train, y_train, n_more_iter=step_size)
+    y_pred = fm.predict(X_test)
+
+    rmse_train.append(np.sqrt(mean_squared_error(fm.predict(X_train), y_train)))
+    rmse_test.append(np.sqrt(mean_squared_error(fm.predict(X_test), y_test)))
+
+    r2_score_train.append(r2_score(fm.predict(X_train), y_train))
+    r2_score_test.append(r2_score(fm.predict(X_test), y_test))
+
+
+from matplotlib import pyplot as plt
+fig, axes = plt.subplots(ncols=2, figsize=(15, 4))
+
+x = np.arange(1, n_iter) * step_size
+with plt.style.context('fivethirtyeight'):
+    axes[0].plot(x, rmse_train, label='RMSE-train', color='r', ls="--")
+    axes[0].plot(x, rmse_test, label='RMSE-test', color='r')
+    axes[1].plot(x, r2_score_train, label='R^2-train', color='b', ls="--")
+    axes[1].plot(x, r2_score_test, label='R^2-test', color='b')
+axes[0].set_ylabel('RMSE', color='r')
+axes[1].set_ylabel('R^2', color='b')
+axes[0].legend()
+axes[1].legend()
---
+++ index.rst
@@ -0,0 +1,19 @@
+.. fastFM documentation master file, created by
+   sphinx-quickstart on Mon Jun 15 01:42:19 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to fastFM's documentation!
+==================================
+
+This is the documentation for fastFM's python interface.
+**Source code** and **install instructions** can be found on https://github.com/ibayer/fastFM.
+A short paper describing the library is available on arXiv http://arxiv.org/abs/1505.00641
+
+.. toctree::
+   :maxdepth: 3
+
+   tutorial
+   guide
+   api
+
