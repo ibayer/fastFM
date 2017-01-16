509265bea0f5d2d2ad12dc823b1e83ee6f59f7ba
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2017-01-16 23:19:20 +0100

    push minor version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
 
     package_data={'fastFM': ['fastFM/*.pxd']},
 
-    version='0.2.6',
+    version='0.2.6.2',
     url='http://ibayer.github.io/fastFM',
     author='Immanuel Bayer',
     author_email='immanuel.bayer@uni-konstanz.de',
