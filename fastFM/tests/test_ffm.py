0fc8dc43f0aa90addd17d576e3f7bb7bd2a3f566
AlexJoz <yakimbee@gmail.com>
2017-11-15 19:30:52 +0200

    move init in test, use simple assert

--- fastFM/tests/test_ffm.py
+++ fastFM/tests/test_ffm.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 import scipy.sparse as sp
-from nose.tools import assert_true
 from numpy.testing import assert_equal
 from sklearn.metrics import mean_squared_error
 
@@ -21,18 +20,6 @@ def get_test_problem():
                   [5, 8]], dtype=np.float64)
     w = np.array([9, 2], dtype=np.float64)
     w0 = 2
-    return w0, w, V, y, X
-
-def get_another_test_problem():
-    X = sp.csc_matrix(np.array([[6, 1],
-                                [2, 3],
-                                [3, 0],
-                                [6, 1],
-                                [4, 5]]), dtype=np.float64)
-    y = np.array([298, 266, 29, 298, 848], dtype=np.float64)
-    V = np.random.normal(loc=0.0, scale=1.0, size=(2, 2))
-    w = np.array([0, 0], dtype=np.float64)
-    w0 = 0
     return w0, w, V, y, X
 
 def test_ffm_predict():
@@ -53,7 +40,11 @@ def test_ffm2_predict_w0():
     assert_equal(y_pred, w0)
 
 def test_ffm2_fit():
-    w0, w, V, y, X = get_another_test_problem()
+    w0, w, V, y, X = get_test_problem()
+    w0 = 0
+    w[:] = 0
+    V = np.random.normal(loc=0.0, scale=1.0, size=(2, 2))
+
     w0_init = w0
     w_init = np.copy(w)
     V_init = np.copy(V)
@@ -67,7 +58,8 @@ def test_ffm2_fit():
     y_pred = ffm2.ffm_predict(w0, w, V, X)
     msqr_after = mean_squared_error(y, y_pred)
 
-    assert_true(w0 != w0_init)
-    assert_true(np.any(np.not_equal(w, w_init)))
-    assert_true(np.any(np.not_equal(V, V_init)))
-    assert_true(msqr_before > msqr_after)
+    assert(w0 != w0_init)
+    # FIXME: use np.all instead np.any after we can set solver params from python
+    assert(np.any(np.not_equal(w, w_init)))
+    assert(np.any(np.not_equal(V, V_init)))
+    assert(msqr_before > msqr_after)
